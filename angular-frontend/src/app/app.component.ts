944467cf98cbc72be09cd7770f30e9bbd275543c
raju-jsr <rajuranjancp@gmail.com>
2025-05-29 23:02:40 +0530

    Preparing Home Component and adding Angular Material to project

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HomeComponent]
 })
 export class AppComponent {
   title = 'Rider Paradise';
